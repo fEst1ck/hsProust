1a21256864e30ac2a0959d041a46d9fafb674f25
Zekun Wang <wswzk864864@gmail.com>
2021-03-01 03:01:41 +0800

    Now Lexer only export lexer.

--- Lexer.hs
+++ Lexer.hs
@@ -1,14 +1,11 @@
-module Lexer( lexer ) where
-import Text.Parsec
+module Lexer( hsProustDef, lexer ) where
+import Text.Parsec ( alphaNum, oneOf, (<|>) )
 import Text.Parsec.Language (haskellStyle, LanguageDef)
 import Text.Parsec.Token
-
-
-
 hsProustDef :: LanguageDef st
 hsProustDef = haskellStyle
             {
-                identLetter = alphaNum <|> oneOf "_-'",
+                identLetter = alphaNum <|> oneOf "_'",
                 opLetter = opLetter haskellStyle <|> oneOf "∨∧¬→⊥",
                 reservedOpNames = ["\\/", "∨",
                                    "/\\", "∧",
