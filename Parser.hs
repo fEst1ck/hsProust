fcd59b6a5f3b00bcd86686dad381fe27c6002cdf
Zekun Wang <wswzk864864@gmail.com>
2021-03-01 03:00:24 +0800

    Add Parser.

--- Parser.hs
+++ Parser.hs
@@ -1,7 +1,6 @@
-module Parser where
+module Parser (parseExpr, parseType) where
 
-import Prelude hiding ( sum, not )
-import Lexer ( lexer )
+import Lexer ( hsProustDef, lexer )
 import Text.Parsec
 import Text.Parsec.Combinator
 import Text.Parsec.Expr
@@ -12,8 +11,8 @@ import PrettyPrint
 identifier = Token.identifier lexer
 parens = Token.parens lexer
 reservedOp = Token.reservedOp lexer
-
-var = Var <$> identifier
+symbol = Token.symbol lexer
+whiteSpace = Token.whiteSpace lexer
 
 opArrow = (reservedOp "->" <|> reservedOp "→") >> return Arrow
 
@@ -25,6 +24,12 @@ opNot = (reservedOp "~" <|> reservedOp "¬") >> return (`Arrow` Bot)
 
 opBot = reservedOp "⊥" >> return Bot
 
+-- t = t → t
+--   | t ∨ t
+--   | t ∧ t
+--   | ¬ t
+--   | ⊥
+--   | x
 typeExpr = buildExpressionParser typeOpTable typeTerm
 
 typeTerm =  parens typeExpr
@@ -38,18 +43,45 @@ typeOpTable = [
                [Infix opArrow AssocRight]
               ]
 
-arrow :: Parsec String () Type
--- arrow = Arrow <$> prod <* opArrow *> arrow <|> prod
-arrow = chainl1 typeVar opArrow
-prod  = Sum <$> arrow <* opOr *> sum <|> sum
-sum = Product <$> sum <* opAnd *> not <|> not
--- not = (opNot >> bot >>= (`Arrow` Bot)) <|> bot
-not = (\_ y -> Arrow y Bot) <$> opNot *> bot <|> bot
-bot = (reservedOp "⊥" >> return Bot) <|> typeVar
 typeVar = TypeVar <$> identifier
 
-types = undefined
+lam = do {reservedOp "\\"; x <- identifier; reservedOp "=>"; e <- expr'''; return $ Lam x e}
+-- lam = reservedOp "\\" >> Lam <$> identifier <*> expr'''
+andIntro = symbol "/\\-intro" >> AndIntro <$> expr''' <*> expr'''
+andElim0 = symbol "/\\-elim0" >> AndElim0 <$> expr'''
+andElim1 = symbol "/\\-elim1" >> AndElim1 <$> expr'''
+orIntro0 = symbol "\\/-intro0" >> OrIntro0 <$> expr'''
+orIntro1 = symbol "\\/-intro1" >> OrIntro1 <$> expr'''
+orElim = symbol "\\/-elim" >> OrElim <$> expr''' <*> expr''' <*> expr'''
+botElim = symbol "⊥" >> BotElim <$> expr'''
+ann = do { x <- expr'; reservedOp ":"; t <- typeExpr; return $ Ann x t }
+hole = reservedOp "?" >> return (Hole 0)
+-- app = do {f <- expr'; appSym; a <- expr; return $ App f a}
+app = chainl1 expr (appSym >> return App)
+appSym = whiteSpace >> notFollowedBy (choice . map reservedOp $ Token.reservedOpNames hsProustDef)
+-- ann = chainr1 expr' (reservedOp ":" >> return $ Ann x t)
+var = Var <$> identifier
 
 expr :: Parsec String () Expr
-expr =  parens expr
-    <|> undefined
+expr =  andIntro
+    <|> andElim0
+    <|> orIntro0
+    <|> orIntro1
+    <|> orElim
+    <|> botElim
+    <|> hole
+    <|> var
+    <|> parens expr'''
+
+expr' :: Parsec String () Expr
+expr' = app <|> expr <|> parens expr''
+
+expr'' :: Parsec String () Expr
+expr'' = try ann <|> expr'
+      <|> parens expr'''
+
+expr''' = lam <|> expr'' <|> parens expr'''
+
+parseExpr = expr'''
+
+parseType = typeExpr
