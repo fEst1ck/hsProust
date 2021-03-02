f2a404a5206b50357be69fec5eca2d1bf5170abc
Zekun Wang <wswzk864864@gmail.com>
2021-03-02 09:52:27 +0800

    Add support for unicode symbol.

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -8,14 +8,14 @@ hsProustDef = haskellStyle
             {
                 identStart = identStart haskellStyle <|> char '_',
                 identLetter = alphaNum <|> oneOf "_'",
-                opLetter = opLetter haskellStyle <|> oneOf "∨∧¬→⊥",
+                opLetter = opLetter haskellStyle <|> oneOf "∨∧¬→⊥λ",
                 reservedOpNames = ["\\/", "∨",
                                    "/\\", "∧",
                                    "~", "¬",
                                    "->", "→",
                                    "⊥",
                                    ":",
-                                   "\\", "=>"]
+                                   "\\", "λ", "=>"]
             }
 
 lexer = makeTokenParser hsProustDef
--- src/Parser.hs
+++ src/Parser.hs
@@ -45,14 +45,14 @@ typeOpTable = [
 
 typeVar = TypeVar <$> identifier
 
-lam = do {reservedOp "\\"; x <- identifier; reservedOp "=>"; e <- expr'''; return $ Lam x e}
+lam = do {reservedOp "\\" <|> reservedOp "λ"; x <- identifier; reservedOp "=>"; e <- expr'''; return $ Lam x e}
 -- lam = reservedOp "\\" >> Lam <$> identifier <*> expr'''
-andIntro = symbol "/\\-intro" >> AndIntro <$> expr''' <*> expr'''
-andElim0 = symbol "/\\-elim0" >> AndElim0 <$> expr'''
-andElim1 = symbol "/\\-elim1" >> AndElim1 <$> expr'''
-orIntro0 = symbol "\\/-intro0" >> OrIntro0 <$> expr'''
-orIntro1 = symbol "\\/-intro1" >> OrIntro1 <$> expr'''
-orElim = symbol "\\/-elim" >> OrElim <$> expr''' <*> expr''' <*> expr'''
+andIntro = (symbol "/\\-intro" <|> symbol "∧-intro") >> AndIntro <$> expr''' <*> expr'''
+andElim0 = (symbol "/\\-elim0" <|> symbol "∧-elim0") >> AndElim0 <$> expr'''
+andElim1 = (symbol "/\\-elim1" <|> symbol "∧-elim1") >> AndElim1 <$> expr'''
+orIntro0 = (symbol "\\/-intro0" <|> symbol "∨-intro0") >> OrIntro0 <$> expr''' -- ∨
+orIntro1 = (symbol "\\/-intro1" <|> symbol "∨-intro1") >> OrIntro1 <$> expr'''
+orElim = (symbol "\\/-elim" <|> symbol "∨-elim") >> OrElim <$> expr''' <*> expr''' <*> expr'''
 botElim = symbol "⊥" >> BotElim <$> expr'''
 ann = do { x <- expr'; reservedOp ":"; t <- typeExpr; return $ Ann x t }
 hole = reservedOp "?" >> return (Hole 0)
